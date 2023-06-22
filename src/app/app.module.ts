5b61de41ea183805a51b2c986e0b24f2680e28dc
estefaniaperrone <perroneviandas@gmail.com>
2023-06-22 19:10:15 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+
+import { MyPComponent } from './componentes/my-p/my-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MyPComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/my-p/my-p.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-p',
+  template: `
+    <p style="font-weight: 200; text-decoration:underline; font-family:Arial">
+    <ng-content></ng-content>
+    
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyPComponent {
+
+}
